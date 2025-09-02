e13b414dd9e06dcba9e34cb1aa6151083a36138b
Amar Ahirvar <ahirwaramar685@gmail.com>
2025-09-02 17:59:47 +0530

    Sync local code with GitHub

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(
     }),
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 din
-      secure: false, // agar https enable ho to true karna
+      secure: false, // agar https enable ho 
       httpOnly: true,
     },
   })
